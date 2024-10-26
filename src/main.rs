25ccbc904e217cc4802965270f392e7205bf7d84
phil <philip.barlow@hidglobal.com>
2024-10-26 08:24:44 +0000

    blinky

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-fn main() {
+use anyhow::Result;
+use esp_idf_svc::hal::delay::FreeRtos;
+use esp_idf_svc::hal::gpio::PinDriver;
+use esp_idf_svc::hal::prelude::Peripherals;
+
+fn main() -> Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
@@ -7,4 +12,27 @@ fn main() {
     esp_idf_svc::log::EspLogger::initialize_default();
 
     log::info!("Hello, world!");
+
+    let peripherals = Peripherals::take().unwrap();
+
+    let mut led_green = PinDriver::output(peripherals.pins.gpio14)?;
+    let mut led_blue = PinDriver::output(peripherals.pins.gpio13)?;
+    let mut led_red = PinDriver::output(peripherals.pins.gpio12)?;
+
+    loop {
+        led_green.set_high()?;
+        led_blue.set_low()?;
+        led_red.set_low()?;
+        FreeRtos::delay_ms(500);
+
+        led_green.set_low()?;
+        led_blue.set_high()?;
+        led_red.set_low()?;
+        FreeRtos::delay_ms(500);
+
+        led_green.set_low()?;
+        led_blue.set_low()?;
+        led_red.set_high()?;
+        FreeRtos::delay_ms(500);
+    }
 }
