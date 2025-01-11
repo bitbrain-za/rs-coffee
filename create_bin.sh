adfdb09f98897cebd0149048358084b69e97fe00
phil <philip.barlow@hidglobal.com>
2025-01-11 16:07:36 +0000

    updates flashlayout

---
+++ create_bin.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [ $# -ne 1 ] || [[ ! "$1" =~ ^(debug|release)$ ]]; then
+    echo "Usage: $0 [debug|release]"
+    exit 1
+fi
+
+BUILD_TYPE=$1
+
+if [ "$BUILD_TYPE" == "debug" ]; then
+    echo "Building debug image"
+    cargo build --features=device_nvs
+elif [ "$BUILD_TYPE" == "release" ]; then
+    echo "Building release image"
+    cargo build --release --features=device_nvs
+fi
+
+echo "Generating new partition table"
+espflash partition-table my_partitions.csv -o part.bin --to-binary
+
+echo "Original $BUILD_TYPE partition table"
+espflash partition-table "/home/esp/rs_coffee/target/xtensa-esp32s3-espidf/$BUILD_TYPE/partition-table.bin"
+
+echo "Replacing $BUILD_TYPE partition table"
+cp part.bin "/home/esp/rs_coffee/target/xtensa-esp32s3-espidf/$BUILD_TYPE/partition-table.bin"
+
+rm part.bin
+
+echo "New partition table"
+espflash partition-table "/home/esp/rs_coffee/target/xtensa-esp32s3-espidf/$BUILD_TYPE/partition-table.bin"
+
+echo "Merging $BUILD_TYPE image"
+
+espflash save-image \
+    --flash-size 16mb \
+    --chip esp32s3 \
+    --merge \
+    --partition-table "target/xtensa-esp32s3-espidf/$BUILD_TYPE/partition-table.bin" \
+    --bootloader "target/xtensa-esp32s3-espidf/$BUILD_TYPE/bootloader.bin" \
+    "target/xtensa-esp32s3-espidf/$BUILD_TYPE/rs-coffee" \
+    "rs_coffee_$BUILD_TYPE.bin"
--- todo.md
+++ todo.md
@@ -3,10 +3,12 @@
  - [ ] Review timer implementation (https://docs.esp-rs.org/esp-idf-svc/esp_idf_svc/timer/index.html)
  - [ ] Runtime WIFI config
  - [ ] Move ADC to I2C ADC (ADS1115)
+ - [ ] OTA
+ - [ ] HA integration
+
  
  # 0.8
  - [ ] Add Availability
- - [ ] OTA
  - [ ] Fix up all the state machines/system state/operation state
  - [ ] Maybe an observer patter?
  - [ ] SD Card - Wait for next release: https://github.com/esp-rs/esp-idf-svc/issues/467
